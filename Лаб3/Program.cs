0625d754ac1fe0f97092c7f9caac03247449c305
Васенина Ульяна <ulyanawasenina@icloud.com>
2018-04-13 13:37:02 +0300

    Добавление типов и названий инвентаря

--- Лаб3/Program.cs
+++ Лаб3/Program.cs
@@ -10,6 +10,18 @@ namespace Лаб3
     {
         static void Main(string[] args)
         {
+            Tipinv Tip1 = new Tipinv("Тренажеры", "инвентарь для тренировок", 1);
+            Tipinv Tip2 = new Tipinv("Компьютеры", "инвентарь для администрирования", 2);
+            Tipinv Tip3 = new Tipinv("Мебель", "прочий инвентарь", 3);
+            Inventar Inv1 = new Inventar("Беговая дорожка", 1, 15000.50, DateTime.Today, Tip1);
+            Inventar Inv2 = new Inventar("Скамья", 2, 10000.00, DateTime.Today, Tip1);
+            Inventar Inv3 = new Inventar("Ноутбук бухгалтера", 3, 20000.00, DateTime.Today, Tip2);
+            Inventar Inv4 = new Inventar("Зеркало", 4, 15000.00, DateTime.Today, Tip3);
+
+            Inv1.GetInfo();
+
+            Console.ReadLine();            
         }
+
     }
 }
